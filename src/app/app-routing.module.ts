6c858b02cf050597ea925a2d95cd09ead4348a13
Micah Innerarity <micah@Micahs-MacBook-Air.local>
2022-05-21 11:42:27 -0400

    upload initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
+import { JobsComponent } from './components/jobs/jobs.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "home", pathMatch: "full" },
+  { path: "home", component: HomeComponent},
+  { path: "jobs", component: JobsComponent},
+  { path: "about", component: AboutComponent},
+  { path: "**", component: NotfoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { JobsComponent } from './components/jobs/jobs.component';
+import { JobDetailsComponent } from './components/job-details/job-details.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    JobsComponent,
+    JobDetailsComponent,
+    NotfoundComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
